12ca90ee9fbce87eaee4dc942ee991805aea57d9
ArcoWirawan <wrnbcknd@gmail.com>
2025-11-12 01:11:20 +0700

    Halo

---
+++ Python-Basic/25-Dictionary.py
@@ -0,0 +1,20 @@
+# List (array) -> Menggunakan Index untuk mengindetifikasi datanya (panggil, edit panggil index)
+
+list01 = ["arwanz", "arwanzxpnet", "xpnet"]
+
+print("Nama Asli gue = ", list01[0])
+
+# DICTIONARY >> Array Assosiative
+# data disimpan berdasarkan key
+
+# can use all data type
+dict01 = {
+    # identifier harus menggunakan kutib
+    "key1":"value1",
+    "nama":"arwanzxp", # setiap ada data baru, koma ya
+    "kelas":"01TPLM004",
+    'nick':'aarwannzxp' # kutib beb
+}
+
+print(f"Hi, My name is {dict01['nama']}")
+
